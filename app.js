b900674a3aa15d3e100b766e42352672a1e2060c
you kinjoh <you.kinjoh@gmail.com>
2014-04-16 11:00:25 +0900

    first commit.

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , WebSocketServer = require('ws').Server;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var server = http.createServer(app);
+var wsserver = new WebSocketServer({
+  'server': server,
+  'path': '/'
+});
+
+var conns = [];
+
+wsserver.on('connection', function(ws) {
+  conns.push(ws);
+  ws.on('message', function(message) {
+    conns.forEach(function(conn) {
+        try {
+          if (conn !== ws) {
+            conn.send(message);
+          }
+        } catch(e) {
+        }
+    });
+  });
+  ws.on('close', function() {
+    conns.splice(downstreams.indexOf(ws), 1);
+  });
+});
+
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
