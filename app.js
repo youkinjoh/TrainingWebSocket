41ce8ee4ae19629a7310fcfaec4b42b5165f707d
TAW-Memento <Alan@AT-Pluto.local>
2014-04-17 10:15:03 +0900

    correspond to expressv4.0.0 #5
    - add follow package to package.json
     * morgan
     * errorhandler
    - change logger and error_handler to morgan and errorhandler in app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var express = require('express')
   , http = require('http')
   , path = require('path')
+  , morgan = require('morgan')
+  , errorHandler = require('errorhandler')
   , WebSocketServer = require('ws').Server;
 
 var app = express();
 
+
+
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.logger('dev'));
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 var server = http.createServer(app);
